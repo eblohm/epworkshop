71c526cc07661f9c30b3ea4c5634c18a03da8361
Eric Blohm <eblohm93@gmail.com>
2021-03-13 17:14:46 -0500

    update slick stylesheet dependencies

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -35,13 +35,13 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             type="text/css"
-            charset="UTF-8"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+            charSet="UTF-8"
+            href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css"
           />
           <link
             rel="stylesheet"
             type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+            href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.min.css"
           />
         </Head>
         <body>
